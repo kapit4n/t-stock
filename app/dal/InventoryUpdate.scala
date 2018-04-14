bcbb7a2588851c44dcb5cf14229cd29758ded04d
larce <luis.arce22@gmail.com>
2018-04-13 21:54:48 -0400

    Updated shopping cart styles

--- app/dal/InventoryUpdate.scala
+++ app/dal/InventoryUpdate.scala
@@ -4,8 +4,7 @@ class InventoryUpdate {
 
   def updateInventory(productId: Long, quantity: Int, inventoryMethod: Int) {
     if (inventoryMethod == 0) {
-      val updateMethod = new FIFOInventoryUpdate()
-      println("Update Inventory with FIFO " + updateMethod.updateInventory(productId, inventoryMethod))
+      println("Update Inventory with FIFO ")
     } else {
       println("Update Inventory with 0")
     }
